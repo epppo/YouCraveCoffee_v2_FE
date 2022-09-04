bb982b3a2fe561b83ca26aee37288c280ff5fb5f
epppo <amorosso@kakao.com>
2022-09-05 01:09:45 +0900

    Create : - Header 컴포넌트 생성 - BasicModal 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Router from "./Router";
 import Header from "./component/common/Header";
 import styled from "styled-components";
+
 function App() {
   return (
     <Wrap>
@@ -11,4 +12,10 @@ function App() {
 }
 
 export default App;
-const Wrap = styled.div``;
+
+const Wrap = styled.div`
+max-width: 1920px;
+min-width: 400px;
+max-height: 1080px;
+margin: 0 30px;
+`;
--- src/component/common/Header.tsx
+++ src/component/common/Header.tsx
@@ -1,4 +1,38 @@
+import styled from "styled-components";
+import {Coffee} from "../../styles/assets"
+
 const Header = () => {
-  return <div>으잉?</div>;
+  return (
+    <Wrap>
+      <Logo>You Crave Coffee <Coffee/></Logo>
+      <ButtonBox>
+        <div>Login</div>
+        <div>Join</div>
+      </ButtonBox>
+    </Wrap>
+  );
 };
 export default Header;
+
+const Wrap = styled.div`
+  max-width: 1200px;
+  min-width: 400px;
+  height: 50px;
+  border-bottom: 1px solid rosybrown;
+  font-family: "Amatic SC", cursive;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1rem;
+  color: rosybrown;
+`;
+
+const Logo = styled.div`
+  font-size: 2.5rem;
+`;
+
+const ButtonBox = styled.div`
+display: flex;
+gap: 20%;
+font-size: 1.5rem;
+`;
