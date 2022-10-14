aefca6552261833013eda62f06579ae13cc3a18e
epppo <amorosso@kakao.com>
2022-10-14 23:13:57 +0900

    Create : 메인페이지 커피 이미지 정적파일화 / 랭크 컴포넌트 생성

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,4 +1,22 @@
-const Main = () =>{
-  return(<div>오잉</div>)
-}
+import MainImg from "../styles/assets/img/mainImg.png";
+import styled from "styled-components";
+import MonthRank from "../component/main/MonthRank";
+
+const Main = () => {
+  return (
+    <>
+      <ImgWrap>
+        <img src={MainImg} />
+      </ImgWrap>
+      <MonthRank/>
+    </>
+  );
+};
 export default Main;
+
+const ImgWrap = styled.div`
+  padding: 10px 0;
+  img {
+    width: 100%;
+  }
+`;
